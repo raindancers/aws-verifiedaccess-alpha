2f76a6a7e623f888cc857d54c7e779aa1aab9404
Andrew Frazer <andrew.frazer@raindancers.cloud>
2023-07-16 02:55:07 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Andrew Frazer',
+  authorAddress: 'andrew.frazer@raindancers.cloud',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'aws-verifiedaccess-alpha',
+  projenrcTs: true,
+  repositoryUrl: 'https://mrpackethead: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@github.com/raindancers/aws-verifiedaccess-alpha.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
