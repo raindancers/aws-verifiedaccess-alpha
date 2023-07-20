4eead17f07f0bee1ae2605d32d47d3159ca8720a
Andrew Frazer <andrew.frazer@raindancers.cloud>
2023-07-20 05:07:04 +0000

    feat: Publish to constructs.dev

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,11 +3,16 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Andrew Frazer',
   authorAddress: 'andrew.frazer@raindancers.cloud',
   cdkVersion: '2.87.0',
+  keywords: ['aws-cdk', 'cdk', 'verfiedaccess'],
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
   name: 'aws-verifiedaccess-alpha',
   projenrcTs: true,
   repositoryUrl: 'https://mrpackethead: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@github.com/raindancers/aws-verifiedaccess-alpha.git',
+  publishToPypi: {
+    distName: 'raindancers',
+    module: 'aws-verifiedaccess-alpha',
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
